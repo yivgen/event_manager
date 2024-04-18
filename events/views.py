18cf75cc2d53510ff9a393bcf4303abd19231a3a
evgene020818 <evgene.020818@gmail.com>
2024-04-18 17:40:42 +0300

    Added basic event search based on title and description

--- events/views.py
+++ events/views.py
@@ -3,10 +3,20 @@ from events.models import Event
 from events.serializers import EventSerializer, RegisterUserForEventSerializer
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
+
 
 class EventList(generics.ListCreateAPIView):
-    queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            return Event.objects.filter(
+                Q(title__icontains=query) | Q(description__icontains=query)
+            )
+
+        return Event.objects.all()
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
